6424ef8bb8158c9d22322b94c38bda450325cf68
MstrJL300 <joslum3@icloud.com>
2016-11-14 22:56:54 -0500

    Agregado: Formato de foto, Ventana de dialogo

--- Codelab2/app/src/main/java/com/example/jose/codelab2/MainActivity.java
+++ Codelab2/app/src/main/java/com/example/jose/codelab2/MainActivity.java
@@ -1,22 +1,62 @@
 package com.example.jose.codelab2;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import static android.R.attr.tag;
 
 public class MainActivity extends AppCompatActivity {
 
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    private ImageView mImageView;
+    Bitmap imageBitmap;
+    private static final int ACTION_TAKE_PHOTO_S = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //PHOTO
+        mImageView = (ImageView) findViewById(R.id.imageView1);
+        imageBitmap = null;
+
+        //SAVE
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    /** Called when the user clicks the Send button */
+    //MESSAGE SECTION
+
+    /**
+     * Called when the user clicks the Send button
+     */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
@@ -25,4 +65,44 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //PHOTO SECTION
+
+    /**
+     * Called to call an action that takes the photo.
+     */
+    private void dispatchTakePictureIntent() {
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null)
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    /**
+     * Called to retrieve the image and display an ImageView
+     * @param requestCode
+     * @param resultCode
+     * @param data  Is only use to control small images
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    /**
+     * Called when the user clicks the Photo button
+     */
+    public void sendPhoto(View view) {
+        dispatchTakePictureIntent();
+    }
+
+    //SAVE SECTION
+    public void save(View view) {
+        DialogFragment newFragment = new FireMissilesDialogFragment();
+        newFragment.show(getSupportFragmentManager(), "missiles");
+    }
 }
