2bd435c0b077e42ef9d236e13ed7b66b63b6ab93
MstrJL300 <joslum3@icloud.com>
2016-11-16 11:22:36 -0500

    La ventana de alerta aparece cuando no se encuentra la foto y el texto

--- Codelab2/app/src/main/java/com/example/jose/codelab2/MainActivity.java
+++ Codelab2/app/src/main/java/com/example/jose/codelab2/MainActivity.java
@@ -1,27 +1,18 @@
 package com.example.jose.codelab2;
 
-import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.v4.app.DialogFragment;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 
-import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
-
-import static android.R.attr.tag;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,18 +23,32 @@ public class MainActivity extends AppCompatActivity {
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
+
+    //MESSAGE
+    Intent intent;
+    EditText editText;
+    //PHOTO
     private ImageView mImageView;
     Bitmap imageBitmap;
     private static final int ACTION_TAKE_PHOTO_S = 2;
+    Intent takePictureIntent;
+
+    //SAVE
+    DialogFragment newFragment = new MessageDialogFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //MESSAGE
+        intent = new Intent(this, DisplayMessageActivity.class);
+//        editText = (EditText) findViewById(R.id.edit_message);
         //PHOTO
         mImageView = (ImageView) findViewById(R.id.imageView1);
         imageBitmap = null;
+        takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
         //SAVE
 
@@ -58,8 +63,7 @@ public class MainActivity extends AppCompatActivity {
      * Called when the user clicks the Send button
      */
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
+        editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
@@ -71,9 +75,6 @@ public class MainActivity extends AppCompatActivity {
      * Called to call an action that takes the photo.
      */
     private void dispatchTakePictureIntent() {
-
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
         if (takePictureIntent.resolveActivity(getPackageManager()) != null)
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
     }
@@ -101,8 +102,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //SAVE SECTION
+
+    /**
+     * Called when the user clicks the Save button
+     */
     public void save(View view) {
-        DialogFragment newFragment = new FireMissilesDialogFragment();
-        newFragment.show(getSupportFragmentManager(), "missiles");
+        editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+//        System.out.println("|||"+message.isEmpty());
+        if(imageBitmap == null && message.isEmpty())
+            newFragment.show(getSupportFragmentManager(), "misiles"); //Shows the dialog
     }
 }
--- Codelab2/app/src/main/java/com/example/jose/codelab2/MessageDialogFragment.java
+++ Codelab2/app/src/main/java/com/example/jose/codelab2/MessageDialogFragment.java
@@ -5,18 +5,39 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
 
 /**
  * Created by jose on 14/11/16.
  */
 
-public class FireMissilesDialogFragment extends DialogFragment {
+public class MessageDialogFragment extends android.support.v4.app.DialogFragment {
+
+    /**
+     * When MessageDialogFragment is created in MainActivity it uses "onCreateDialog" to
+     * make the content of the dialog.
+     * @param savedInstanceState
+     * @return
+     */
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage(R.string.dialog_fire_missiles)
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+
+        builder.setView(inflater.inflate(R.layout.message_dialog_fragment, null));
+
+        //This is another way to set the message for the builder.
+//        builder.setMessage(R.string.dialog_fire_missiles);
+                /*
+                This are aditional atributes for builder in the form of buttons, I'm not using
+                them for this app.
+
                 .setPositiveButton(R.string.fire, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         // FIRE ZE MISSILES!
@@ -26,7 +47,7 @@ public class FireMissilesDialogFragment extends DialogFragment {
                     public void onClick(DialogInterface dialog, int id) {
                         // User cancelled the dialog
                     }
-                });
+                });*/
         // Create the AlertDialog object and return it
         return builder.create();
     }
